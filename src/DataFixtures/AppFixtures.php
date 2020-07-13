d2096f2fa15841ce1c46ae4fe67d41e776a51405
cnvvcvlad <cnvvc_vlad@yahoo.fr>
2020-07-13 19:03:20 +0200

    configuration and generating datafixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Article;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,12 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        for ($count = 0; $count < 20; $count++) {
+            $article = new Article;
+            $article->setTitle("Titre " . $count);
+            $article->setUri("Uri Fixture" . $count);
+            $manager->persist($article);
+        }
         $manager->flush();
     }
 }
