9e9acda180e0543ead4adfbd731f1da2731f70fb
cnvvcvlad <cnvvc_vlad@yahoo.fr>
2020-07-13 18:26:22 +0200

    create datafixture and article entity

---
+++ src/DataFixtures/ArticleFixture.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ArticleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        for ($count = 0; $count < 20; $count++) {
+            $article = new Article;
+            $article->setTitle("Titre " . $count);
+            $article->setUri("Uri Fixture" . $count);
+            $manager->persist($article);
+        }
+        $manager->flush();
+    }
+}
